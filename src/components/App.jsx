1d97b9ecf43e13f0f1c2ff080475d7c8c6c587c8
Colin Huber <cbhuber@gmail.com>
2023-12-26 18:53:15 -0700

    fix: routing errors.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { CssBaseline } from "@mui/material";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import useStyles from "./styles";
 // import useAlan from "./Alan";
@@ -19,20 +19,12 @@ const App = () => {
       {/* <NavBar /> */}
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Switch>
-          <Route exact path="/movie/:id">
-            {/* <MovieInformation /> */}
-          </Route>
-          <Route exact path="/actors/:id">
-            {/* <Actors /> */}
-          </Route>
-          <Route exact path={["/", "/approved"]}>
-            {/* <Movies /> */}
-          </Route>
-          <Route exact path="/profile/:id">
-            {/* <Profile /> */}
-          </Route>
-        </Switch>
+        <Routes>
+          {/* <Route exact path="/movie/:id" element={<MovieInformation />} /> */}
+          {/* <Route exact path="/actors/:id" element={<Actors />} /> */}
+          {/* <Route exact path={["/", "/approved"]} element={<Movies />} /> */}
+          {/* <Route exact path="/profile/:id" element={<Profile />} /> */}
+        </Routes>
       </main>
       <div ref={alanBtnContainer} />
     </div>

ea0e18bad45641471d0d0cea10f7bbf3f5ea24a2
Colin Huber <cbhuber@gmail.com>
2023-12-26 18:27:03 -0700

    feat: Main entry to app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ToggleColorModeProvider from "./utils/ToggleColorMode";
+import App from "./components/App";
+// import store from "./app/store";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <Provider store={store}>
+  <ToggleColorModeProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ToggleColorModeProvider>
+  // </Provider>
+);

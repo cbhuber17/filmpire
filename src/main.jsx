15cc6c6b2ce89581f70c585c920315199f189006
Colin Huber <cbhuber@gmail.com>
2023-12-26 18:49:12 -0700

    fix: Import createRoot properly.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 
@@ -8,7 +9,7 @@ import App from "./components/App";
 // import store from "./app/store";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
   <ToggleColorModeProvider>
     <BrowserRouter>

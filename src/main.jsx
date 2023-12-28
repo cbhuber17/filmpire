e3830f21635b1b7c647313d46688d16de8432743
Colin Huber <cbhuber@gmail.com>
2023-12-27 19:07:54 -0700

    feat: The Movie DataBase redux service.

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
 import ToggleColorModeProvider from "./utils/ToggleColorMode";
 import App from "./components/App";
-// import store from "./app/store";
+import store from "./app/store";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  // <Provider store={store}>
-  <ToggleColorModeProvider>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </ToggleColorModeProvider>
-  // </Provider>
+  <Provider store={store}>
+    <ToggleColorModeProvider>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </ToggleColorModeProvider>
+  </Provider>
 );

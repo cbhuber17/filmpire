d49a4de7f74e43d06a3756e6010507cdd53615a7
Colin Huber <cbhuber@gmail.com>
2023-12-29 14:36:44 -0700

    feat: Actors page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import useStyles from "./styles";
 // import useAlan from "./Alan";
 
-import {
-  // Actors,
-  MovieInformation,
-  Movies,
-  NavBar,
-  Profile,
-} from ".";
+import { Actors, MovieInformation, Movies, NavBar, Profile } from ".";
 
 const App = () => {
   const classes = useStyles();
@@ -27,7 +21,7 @@ const App = () => {
         <div className={classes.toolbar} />
         <Routes>
           <Route exact path="/movie/:id" element={<MovieInformation />} />
-          {/* <Route exact path="/actors/:id" element={<Actors />} /> */}
+          <Route exact path="/actors/:id" element={<Actors />} />
           <Route path="/" element={<Movies />} />
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
